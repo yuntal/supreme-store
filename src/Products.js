044704dd8bdad980d96c4fc80e4b35e2588a4a16
Yunus <57660581+yuntal@users.noreply.github.com>
2023-08-14 23:02:19 +0200

    Add files via upload

---
+++ src/Products.js
@@ -0,0 +1,85 @@
+export const PRODUCTS = [
+
+
+    {
+        id: 1, 
+        name: 'Supreme Poster',
+        price: '129.0',
+        image: "https://source.unsplash.com/3KzQ81187xs",
+    },
+
+    {
+        id: 2, 
+        name: 'Supreme classic logo tee',
+        price: '129.0',
+        image: "https://source.unsplash.com/LQrRJjS-q10",
+    },
+
+    {
+        id: 3, 
+        name: 'Supreme x Woody',
+        price: '2799.0',
+        image: "https://source.unsplash.com/nQVx_SPprz0",
+    },
+
+    {
+        id: 4, 
+        name: 'Supreme x LV weeknd bag',
+        price: '6999.0',
+        image: "https://source.unsplash.com/HtaxUxO2vbY",
+    },
+
+
+
+
+    {
+        id: 5, 
+        name: 'Supreme Inflatable',
+        price: '69.0',
+        image: "https://source.unsplash.com/yzDWDcomtpw",
+    },
+    {
+        id: 6, 
+        name: 'Supreme Notepad',
+        price: '19.0',
+        image: "https://source.unsplash.com/wnY0y61B8IM",
+    },
+    {
+        id: 7, 
+        name: 'Supreme Step-Ladder',
+        price: '2999.0',
+        image: "https://source.unsplash.com/OLjdTYtK1_4",
+    },
+    {
+        id: 8, 
+        name: 'Supreme box logo gloves',
+        price: '299.0',
+        image: "https://source.unsplash.com/pHiBA0oHhWs",
+    },
+    {
+        id: 9, 
+        name: 'Supreme deck of cards',
+        price: '129.0',
+        image: "https://source.unsplash.com/saitqe6-Oyo",
+    }, 
+    {
+        id: 10, 
+        name: 'Supreme x Murica',
+        price: '129.0',
+        image: "https://source.unsplash.com/Id2z6AOQ3Xw",
+    },
+    {
+        id: 11, 
+        name: 'Supreme x Everlast',
+        price: '2799.0',
+        image: "https://source.unsplash.com/yYQwAKTN_34",
+    },
+
+    {
+        id: 12, 
+        name: 'Supreme x le noble art',
+        price: '29.0',
+        image: "https://source.unsplash.com/0cAuOkTYVpo",
+    },
+];
+
